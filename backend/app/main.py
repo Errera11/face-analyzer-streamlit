088d21f45922b7c56dc90a8f5ee911ca32ae0175
Ilnaz <ilnaz@Ilnazs-MacBook-Air.local>
2026-05-07 18:58:04 +0300

    chore: add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,17 @@ from fastapi import FastAPI
 from routes.video import router as video_router
 from routes.health import router as health_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     video_router,
