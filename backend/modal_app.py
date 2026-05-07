afc9d681632dec0b9cf6e203f53941642285d408
Ilnaz <ilnaz@Ilnazs-MacBook-Air.local>
2026-05-07 20:10:05 +0300

    fix: save deps

--- backend/modal_app.py
+++ backend/modal_app.py
@@ -1,23 +1,25 @@
 import modal
-
-from app.main import app as fastapi_app
-
-app = modal.App("my-ai-app")
+import os
 
 def get_apt_packages():
+    if not os.path.exists("packages.txt"):
+        return []
     with open("packages.txt", "r") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
+apt_deps = get_apt_packages()
+
+app = modal.App("my-ai-app")
+
 image = (
     modal.Image.debian_slim()
-    .apt_install(*get_apt_packages())
+    .apt_install(*apt_deps)
     .pip_install_from_requirements("requirements.txt")
     .add_local_python_source("app")
-    .add_local_python_source("packages.txt")
-    .add_local_python_source("requirements.txt")
 )
 
 @app.function(image=image)
 @modal.asgi_app()
 def api():
+    from app.main import app as fastapi_app
     return fastapi_app
